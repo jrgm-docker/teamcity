ca07de306ef6fa2493dc00278196e643c45e8fc0
John Morrison <jrgmorrison@gmail.com>
2016-11-09 21:37:58 -0800

    upgrade to jdk 8 (take 2)

--- agent/centos/scripts/common.sh
+++ agent/centos/scripts/common.sh
@@ -23,10 +23,11 @@ curl --silent -L http://github.com/micha/jsawk/raw/master/jsawk > /usr/local/bin
 chmod 755 /usr/local/bin/jsawk
 
 # install java jdk
+JDKRPM=jdk-8u112-linux-x64.rpm
 wget --no-verbose --no-cookies \
   --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-  http://download.oracle.com/otn-pub/java/jdk/8u112-b15/jdk-8u112-linux-x64.rpm
-echo "25b37dc93828cc24e2c246cf6f4a5aac  jdk-8u112-linux-x64.rpm" >> MD5SUM
+  http://download.oracle.com/otn-pub/java/jdk/8u112-b15/$JDKRPM
+echo "25b37dc93828cc24e2c246cf6f4a5aac  $JDKRPM" >> MD5SUM
 md5sum -c MD5SUM
-rpm -Uvh jdk-7u76-linux-x64.rpm
-rm -f jdk-7u76-linux-x64.rpm MD5SUM
+rpm -Uvh $JDKRPM
+rm -f $JDKRPM MD5SUM
