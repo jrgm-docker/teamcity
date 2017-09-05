01e0dc090c3d3fd59d203f3f4704329a4ca201c7
John Morrison <jrgmorrison@gmail.com>
2017-09-05 11:20:11 -0700

    server node 6 fwiw

--- server/scripts/common.sh
+++ server/scripts/common.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 set -e
+set -x
 
 # update and install required packages
 yum --quiet -y update
@@ -8,12 +9,12 @@ yum --quiet -y install epel-release
 yum --quiet repolist
 yum --quiet -y install $(cat /scripts/yum-requirements.txt)
 
-# install node 4.x
+# install node 6.x
 cd /var/tmp
 git clone git://github.com/isaacs/nave.git
 cd nave
-git checkout -b v1.0.1-branch v1.0.1
-./nave.sh usemain 4 2>nave-install.log
+git checkout -b v2.2.3-branch v2.2.3
+./nave.sh usemain 6 2>nave-install.log
 ln -s /usr/local/bin/node /usr/bin/node
 ln -s /usr/local/bin/npm /usr/bin/npm
 /usr/local/bin/npm install -g bower
@@ -23,11 +24,12 @@ curl --silent -L http://github.com/micha/jsawk/raw/master/jsawk > /usr/local/bin
 chmod 755 /usr/local/bin/jsawk
 
 # install java jdk
-JDKRPM=jdk-8u112-linux-x64.rpm
+JDKRPM=jdk-8u144-linux-x64.rpm
+KEYVALUE=090f390dda5b47b9b721c7dfaa008135
 wget --no-verbose --no-cookies \
   --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-  http://download.oracle.com/otn-pub/java/jdk/8u112-b15/$JDKRPM
-echo "25b37dc93828cc24e2c246cf6f4a5aac  $JDKRPM" >> MD5SUM
+  http://download.oracle.com/otn-pub/java/jdk/8u144-b01/$KEYVALUE/$JDKRPM
+echo "dcc4c903506766ec4c50a969babdd856  $JDKRPM" >> MD5SUM
 md5sum -c MD5SUM
 rpm -Uvh $JDKRPM
 rm -f $JDKRPM MD5SUM
